4fa714ff6dec131dd6ee17304e6ca9ac8d8b82b1
Antonio Benincasa <benincasaantonio98@gmail.com>
2025-12-09 11:34:09 +0100

    feat: add Firebase configuration and integration with analytics and authentication

---
+++ src/lib/firebase.ts
@@ -0,0 +1,26 @@
+import { getAnalytics, type Analytics } from "firebase/analytics";
+import { initializeApp, getApps, type FirebaseApp } from "firebase/app";
+import { getAuth, type Auth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+};
+
+let app: FirebaseApp;
+let analytics: Analytics;
+if (getApps().length === 0) {
+  app = initializeApp(firebaseConfig);
+  analytics = getAnalytics(app);
+} else {
+  app = getApps()[0];
+}
+
+export const auth: Auth = getAuth(app);
+
+export { app, analytics };
