208ef7642a18964347d9f647dfdd70bc7aad2238
Antonio Benincasa <benincasaantonio98@gmail.com>
2026-01-02 15:50:11 +0100

    feat: remove unused type exports from movie details for cleaner module structure

--- src/features/movie-search/models/index.ts
+++ src/features/movie-search/models/index.ts
@@ -2,10 +2,3 @@ export {
   type TMDBMovieSearchResultItem,
   type TMDBMovieSearchResult,
 } from "./tmdb-movie-search-result";
-export {
-  type TMDBMovieDetails,
-  type TMDBGenre,
-  type TMDBProductionCompany,
-  type TMDBProductionCountry,
-  type TMDBSpokenLanguage,
-} from "./tmdb-movie-details";
