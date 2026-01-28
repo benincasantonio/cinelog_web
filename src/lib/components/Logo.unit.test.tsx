d9703363991e39613a5d38dc2aac2674206f2f6c
Antonio Benincasa <benincasaantonio98@gmail.com>
2026-01-28 07:11:05 +0100

    refactor(unit test): remove unused imports

--- src/lib/components/Logo.unit.test.tsx
+++ src/lib/components/Logo.unit.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
-import { describe, expect, it, vi } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import { Logo } from './Logo';
 
 // Wrapper component for React Router
