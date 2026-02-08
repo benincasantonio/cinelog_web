fe26409a240f5f01052f4e9a17a677f8a2ca73ae
Antonio Benincasa <benincasaantonio98@gmail.com>
2026-02-08 09:36:39 +0100

    feat: implement shallowEqual utility function and corresponding unit tests

---
+++ src/lib/utilities/shallow-equal.ts
@@ -0,0 +1,10 @@
+export const shallowEqual = <T extends object>(objA: T, objB: T): boolean => {
+	const objectAKeys = Object.keys(objA) as (keyof T)[];
+	const objectBKeys = Object.keys(objB) as (keyof T)[];
+
+	if (objectAKeys.length !== objectBKeys.length) {
+		return false;
+	}
+
+	return objectAKeys.every((key) => objA[key] === objB[key]);
+};
